e297673464a0e1f1b60c0634bec40b48ad1310c7
Parker Bartlett <prbartlett1@gmail.com>
2019-03-03 15:28:01 -0500

    gym controller base layout

--- src/main/java/org/dmf/weekendprojectreviewsite/controllers/GymController.java
+++ src/main/java/org/dmf/weekendprojectreviewsite/controllers/GymController.java
@@ -1,5 +1,30 @@
 package org.dmf.weekendprojectreviewsite.controllers;
 
-public class GymController {
+import javax.annotation.Resource;
 
+import org.dmf.weekendprojectreviewsite.repositories.GymRepository;
+import org.dmf.weekendprojectreviewsite.repositories.ReviewRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+// telling spring this is a controller 
+@Controller
+public class GymController {
+	
+	
+	//allowing us to access such repositories
+	@Resource
+	GymRepository gyms;
+	@Resource
+	ReviewRepository reviews;
+	
+	//root mapping
+	@GetMapping ("/")
+	public String home(Model model) {
+		//model attribute method needs a name and collection... which is provided here
+		model.addAttribute("gyms", gyms.findAll());
+		return "home";
+	}
 }
