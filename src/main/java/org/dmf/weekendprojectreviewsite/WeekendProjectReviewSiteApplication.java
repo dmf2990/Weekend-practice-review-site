3cc04ec5f3f3248a5ec6087dd60195ade91b333a
Drew Franklin <drewfranklin@drews-mbp.home>
2019-03-03 14:39:08 -0500

    initial

---
+++ src/main/java/org/dmf/weekendprojectreviewsite/WeekendProjectReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package org.dmf.weekendprojectreviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendProjectReviewSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekendProjectReviewSiteApplication.class, args);
+	}
+
+}
